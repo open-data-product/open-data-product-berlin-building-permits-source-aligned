b21ce7ed7b05f0bfd45be8e91d383f5f2d3a9356
Florian Schwanz <florian.schwanz@interoberlin.de>
2023-11-28 08:35:33 +0100

    chore: add csv converter (including measures on existing buildings) #1

---
+++ lib/transform/data_csv_converter.py
@@ -0,0 +1,76 @@
+import os
+
+import pandas as pd
+
+from lib.tracking_decorator import TrackingDecorator
+
+
+@TrackingDecorator.track_time
+def convert_data_to_csv(source_path, results_path, clean=False, quiet=False):
+    # Iterate over files
+    for subdir, dirs, files in sorted(os.walk(source_path)):
+        # Make results path
+        subdir = subdir.replace(f"{source_path}/", "")
+        os.makedirs(os.path.join(results_path, subdir), exist_ok=True)
+
+        for file_name in [file_name for file_name in sorted(files)
+                          if not file_name.startswith(f"~") and
+                             (file_name.endswith(".xlsx") or file_name.endswith(".xls"))]:
+            source_file_path = os.path.join(source_path, subdir, file_name)
+
+            convert_file_to_csv_completions_new_buildings(source_file_path, clean=clean, quiet=quiet)
+
+
+def convert_file_to_csv_completions_new_buildings(source_file_path, clean=False, quiet=False):
+    source_file_name, source_file_extension = os.path.splitext(source_file_path)
+    file_path_csv = f"{source_file_name}-1-permits-including-measures-on-existing-buildings.csv"
+
+    # Check if result needs to be generated
+    if not clean and os.path.exists(file_path_csv):
+        if not quiet:
+            print(f"✓ Already exists {os.path.basename(file_path_csv)}")
+        return
+
+    # Determine engine
+    engine = build_engine(source_file_extension)
+
+    try:
+        # Iterate over sheets
+        sheet = "Baugen. Tab. 1"
+        skiprows = 10
+        names = ["year", "building_permits_total", "building_permits_residential_buildings",
+                 "building_permits_non_residential_buildings",
+                 "building_measure_on_existing_buildings", "usage_area", "living_area", "apartments",
+                 "apartment_rooms", "estimated_costs"]
+        drop_columns = ["year"]
+
+        dataframe = pd.read_excel(source_file_path, engine=engine, sheet_name=sheet, skiprows=skiprows, names=names,
+                                  index_col=False) \
+            .drop(columns=drop_columns, errors="ignore") \
+            .replace("…", None) \
+            .dropna() \
+            .tail(1)
+
+        # Write csv file
+        write_csv_file(dataframe, file_path_csv, quiet)
+    except Exception as e:
+        print(f"✗️ Exception: {str(e)}")
+
+
+#
+# Helpers
+#
+
+def build_engine(source_file_extension):
+    return "openpyxl" if source_file_extension == ".xlsx" else None
+
+
+def write_csv_file(dataframe, file_path, quiet):
+    if dataframe.shape[0] > 0:
+        dataframe.to_csv(file_path, index=False)
+        if not quiet:
+            print(f"✓ Convert {os.path.basename(file_path)}")
+    else:
+        if not quiet:
+            print(dataframe.head())
+            print(f"✗️ Empty {os.path.basename(file_path)}")
